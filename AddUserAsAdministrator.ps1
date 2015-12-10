9c60bacd3e45dd44ebbae6d4ac31e8126bfde0ba
nboettcher <nboettcher31@gmail.com>
2015-12-10 12:18:31 -0600

    Created AddUserAsAdministrator.ps1 in AzureAutomation.

---
+++ AddUserAsAdministrator.ps1
@@ -0,0 +1,30 @@
+param(
+   [Parameter(Mandatory=$True,Position=1)]
+   [string]$sqlServer,
+	
+   [Parameter(Mandatory=$True)]
+   [string]$database,
+   
+   [Parameter(Mandatory=$True)]
+   [string]$userId,
+   
+   [Parameter(Mandatory=$True)]
+   [string]$email
+)
+
+$sqlServer = $sqlServer + '.database.windows.net'
+
+$sqlCredential = Get-AutomationPSCredential -Name 'SQLCredentials'
+$connectionString = "Data Source=" + $sqlServer + ";Initial Catalog=" + $database + ";User ID=" + $sqlCredential.UserName + ";Password=" + $sqlCredential.GetNetworkCredential().Password + ";Connection Timeout=90"
+$connection = New-Object -TypeName System.Data.SqlClient.SqlConnection($connectionString)
+$query = "INSERT INTO AdmUsers (UserID, UserLogonName, Domain, Email, Active) VALUES ('" + $userId + "', '" + $email + "', 'fpstratus.onmicrosoft.com', '" + $email + "', 1)"
+$command = New-Object -TypeName System.Data.SqlClient.SqlCommand($query, $connection)
+$connection.Open()
+[void]$command.ExecuteNonQuery()
+$connection.Close()
+
+$query = "INSERT INTO AdmUserGroup (UserID, GroupID) VALUES ('" + $userId + "', (SELECT GroupID FROM AdmGroups WHERE Name = 'Administrators'))"
+$command = New-Object -TypeName System.Data.SqlClient.SqlCommand($query, $connection)
+$connection.Open()
+[void]$command.ExecuteNonQuery()
+$connection.Close()
