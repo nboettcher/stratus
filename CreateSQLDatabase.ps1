d7135e4d8e22a89613c2570816807da5d794b7ac
nboettcher <nboettcher31@gmail.com>
2015-12-10 12:29:23 -0600

    Created CreateSQLDatabase.ps1 in AzureAutomation.

---
+++ CreateSQLDatabase.ps1
@@ -0,0 +1,19 @@
+Param(
+  [Parameter(Mandatory=$True,Position=1)]
+  [string]$sqlServer,
+	
+  [Parameter(Mandatory=$True)]
+  [string]$database
+)
+
+$environmentName = $environmentName -replace '\s',''
+$Cred = Get-AutomationPSCredential -name 'AutomationServiceAccount'
+$login = Login-AzureRMAccount -Credential $Cred
+$AzureSubscriptionId = Get-AutomationVariable -Name 'AzureSubscriptionId'
+$account = Add-AzureRmAccount -SubscriptionId $AzureSubscriptionId  -Credential $Cred
+
+$edition = Get-AutomationVariable -Name 'DatabaseEdition'
+$level = Get-AutomationVariable -Name 'DatabaseLevel'
+$resourceGroup = Get-AutomationVariable -Name 'ResourceGroup'
+
+$newDatabase = New-AzureRMSqlDatabase -ResourceGroupName $resourceGroup -ServerName $sqlServer -DatabaseName $database -Edition $edition -RequestedServiceObjectiveName $level
