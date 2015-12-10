98048d5ed744ee829c28a9a329e0911116a81da4
nboettcher <nboettcher31@gmail.com>
2015-12-10 12:28:24 -0600

    Created CreateAADUser.ps1 in AzureAutomation.

---
+++ CreateAADUser.ps1
@@ -0,0 +1,59 @@
+param(
+   [Parameter(Mandatory=$True)]
+   [string]$firstName,
+   
+   [Parameter(Mandatory=$True)]
+   [string]$lastName,
+   
+   [Parameter(Mandatory=$True)]
+   [string]$email
+)
+
+$password = .\GenerateRandomPassword.ps1 -Length 10
+$fullName = $firstName + ' ' + $lastName
+$mailName = $firstName + $lastName
+
+$body = @{
+    "accountEnabled"= "true";
+    "alternativeSignInNamesInfo" = @(           
+        @{
+            "type" = "emailAddress";            
+            "value" = $email
+        }
+    );
+    "creationType"="NameCoexistence";        
+    "displayName"=$fullName;             
+    "mailNickname"=$mailName;                     
+    "passwordProfile" = @{
+        "password"= $password;
+        "forceChangePasswordNextLogin"="true"
+    };
+    "passwordPolicies"="DisablePasswordExpiration";
+}
+
+$json = $body | ConvertTo-Json -depth 2
+$enc = New-Object "System.Text.ASCIIEncoding"
+$byteArray = $enc.GetBytes($json)
+$contentLength = $byteArray.Length
+
+#Gather variables
+$ClientID = Get-AutomationVariable -Name "AADB2CClientId"
+$Secret = Get-AutomationVariable -Name "AADB2CClientSecret"
+$AzureADDomain = Get-AutomationVariable -Name "AADB2CDomain"
+
+#Build the token
+$header = Get-AzureADToken -ClientID $ClientID -Secret $Secret -AzureADDomain $AzureADDomain
+
+try
+{
+	$createdUser = Invoke-RestMethod -Uri "https://graph.windows.net/fpstratus.onmicrosoft.com/users?api-version=beta" -Method Post -Body $json -Headers @{"Authorization"=$header;"Content-Type"="application/json";"Content-Length"=$contentLength}
+	$emailNewUser = .\EmailNewUser.ps1 -toEmail $email -userPassword $password
+
+	return $createdUser.objectId
+}
+catch
+{
+	Write-Output "User already exists"
+}
+
+
