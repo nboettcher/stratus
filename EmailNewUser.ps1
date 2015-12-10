b7c14e09d548647aecb2a153e6ef9676b43f98fa
nboettcher <nboettcher31@gmail.com>
2015-12-10 12:32:24 -0600

    Created EmailNewUser.ps1 in AzureAutomation.

---
+++ EmailNewUser.ps1
@@ -0,0 +1,20 @@
+Param(
+   [Parameter(Mandatory=$True,Position=1)]
+   [string]$toEmail,
+   
+   [Parameter(Mandatory=$True,Position=1)]
+   [string]$userPassword
+)
+
+$sendGridCredential = Get-AutomationPSCredential -Name 'SendGridCredentials'
+$username = $sendGridCredential.UserName
+$password = $sendGridCredential.GetNetworkCredential().Password
+
+$body = "Welcome to Fastpath Assure. You have been granted access to Fastpath Assure. Your credentials are as follows: User: " + $toEmail + " Password: " + $userPassword + " Access Fastpath Assure now!"
+
+$url = "https://api.sendgrid.com/api/mail.send.json"
+$contentType = "application/x-www-form-urlencoded; charset=UTF-8"
+$body = "api_user=" + $username + "&api_key=" + $password + "&to=" + $toEmail + "&subject=Welcome to Fastpath Assure&text=" + $body + "&from=noreply@fastpathassure.com"
+
+$resp = Invoke-RestMethod -Method Post -Uri $url -ContentType $contentType -Body $body
+Write-Output $resp
