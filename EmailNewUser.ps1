3fe4d843667bc93d5cbb737c932d37e67a283600
nboettcher <nboettcher31@gmail.com>
2015-12-16 21:33:30 -0600

    Updated EmailNewUser.ps1 in Azure Automation.

--- EmailNewUser.ps1
+++ EmailNewUser.ps1
@@ -13,11 +13,11 @@ $sendGridCredential = Get-AutomationPSCredential -Name 'SendGridCredentials'
 $username = $sendGridCredential.UserName
 $password = $sendGridCredential.GetNetworkCredential().Password
 
-$body = $name ", Welcome to Fastpath Assure. You have been granted access to Fastpath Assure. Your credentials are as follows: User: " + $toEmail + " Password: " + $userPassword + " Access Fastpath Assure now!"
-
+$text = "$name, You have been granted access to Fastpath Assure. Your credentials are as follows: User: $toEmail Password: $userPassword Access Fastpath Assure now!"
+$html = "$name ,<p />Welcome to Fastpath Assure. You have been granted access to Fastpath Assure. Your credentials are as follows:<p />User: $toEmail<br />Password: $userPassword<p />Access www.fastpathassure.com now!"
 $url = "https://api.sendgrid.com/api/mail.send.json"
 $contentType = "application/x-www-form-urlencoded; charset=UTF-8"
-$body = "api_user=" + $username + "&api_key=" + $password + "&to=" + $toEmail + "&subject=Welcome to Fastpath Assure&text=" + $body + "&from=noreply@fastpathassure.com"
+$body = "api_user=" + $username + "&api_key=" + $password + "&to=" + $toEmail + "&subject=Welcome to Fastpath Assure&html=" + $html + "&from=noreply@fastpathassure.com"
 
 $resp = Invoke-RestMethod -Method Post -Uri $url -ContentType $contentType -Body $body
 Write-Output $resp
