224ad46e2f049484ff3ab50095da21d9ea277a30
nboettcher <nboettcher31@gmail.com>
2015-12-16 21:33:08 -0600

    Updated EmailEnvironmentSuccess.ps1 in Azure Automation.

--- EmailEnvironmentSuccess.ps1
+++ EmailEnvironmentSuccess.ps1
@@ -10,9 +10,10 @@ $sendGridCredential = Get-AutomationPSCredential -Name 'SendGridCredentials'
 $username = $sendGridCredential.UserName
 $password = $sendGridCredential.GetNetworkCredential().Password
 
+$html = "$name,<p />Your new Fastpath Assure environment has been provisioned. If you are a new user to Fastpath Assure, you will be sent an email containg your login (your email address) and your temporary password.<p /> Go to www.fastpathassure.com to login."
 $url = "https://api.sendgrid.com/api/mail.send.json"
 $contentType = "application/x-www-form-urlencoded; charset=UTF-8"
-$body = "api_user=" + $username + "&api_key=" + $password + "&to=" + $toEmail + "&subject=Your new Fastpath Assure environment has been provisioned&text=" + $name + ", Your new Fastpath Assure environment has been provisioned. If you are a new user to Fastpath Assure, you will be sent an email containg your login (your email address) and your temporary password. Go to www.fastpathassure.com to login.&from=noreply@fastpathassure.com"
+$body = "api_user=" + $username + "&api_key=" + $password + "&to=" + $toEmail + "&subject=Your new Fastpath Assure environment has been provisioned&html=" + $html + "&from=noreply@fastpathassure.com"
 
 $resp = Invoke-RestMethod -Method Post -Uri $url -ContentType $contentType -Body $body
 Write-Output $resp
