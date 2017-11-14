d260851eb1d2e8231740329edc4784812cdc47f0
nboettcher <nboettcher31@gmail.com>
2017-11-14 09:17:31 -0600

    Updated CreateUserTenantMappingEntry.ps1 in Azure Automation.

--- CreateUserTenantMappingEntry.ps1
+++ CreateUserTenantMappingEntry.ps1
@@ -3,7 +3,16 @@ param(
    [Guid]$tenantId,
    
    [Parameter(Mandatory=$True)]
-   [Guid]$userId
+   [Guid]$userId,
+
+   [Parameter(Mandatory=$True)]
+   [string]$idp,
+
+   [Parameter(Mandatory=$True)]
+   [string]$email,
+
+   [Parameter(Mandatory=$True)]
+   [string]$idpUserId
 )
 
 $sqlCredential = Get-AutomationPSCredential -Name 'SQLCredentials'
@@ -13,7 +22,17 @@ $database = Get-AutomationVariable -Name 'SqlServerDatabase'
 $sqlCredential = Get-AutomationPSCredential -Name 'SQLCredentials'
 $connectionString = "Data Source=" + $sqlServer + ";Initial Catalog=" + $database + ";User ID=" + $sqlCredential.UserName + ";Password=" + $sqlCredential.GetNetworkCredential().Password + ";Connection Timeout=90"
 $connection = New-Object -TypeName System.Data.SqlClient.SqlConnection($connectionString)
-$query = "INSERT INTO UserTenantMapping (TenantId, UserId) VALUES ('" + $tenantId + "', '" + $userId + "')"
+
+IF([string]::IsNullOrEmpty($userId))
+{
+    $id = [guid]::NewGuid()
+    $query = "INSERT INTO InvitedUsers (Id, IdentityProvider, IdentityUserId, TenantId) VALUES ('" + $id + "', '" + $idp + "', '" + $idpUserId + "', '" + $tenantId + "')"
+}
+else
+{
+    $query = "INSERT INTO UserTenantMapping (TenantId, UserId) VALUES ('" + $tenantId + "', '" + $userId + "')"
+}
+
 $command = New-Object -TypeName System.Data.SqlClient.SqlCommand($query, $connection)
 $connection.Open()
 [void]$command.ExecuteNonQuery()
