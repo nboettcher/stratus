c33bb73b73aab76b4e8c0465ed4e8b4bb26d96a5
nboettcher <nboettcher31@gmail.com>
2018-02-22 09:36:02 -0600

    Updated AddFastpathDEVS.ps1 in Azure Automation.

--- AddFastpathDEVS.ps1
+++ AddFastpathDEVS.ps1
@@ -6,9 +6,9 @@ param
     ) 
 
 
-.\NewAdmin.ps1 -tenantId $tenantId -adminEmailAddress 'miller@gofastpath.com' -adminFirstName 'Chris' -adminLastName 'Miller'
-.\NewAdmin.ps1 -tenantId $tenantId -adminEmailAddress 'nelson.middendorff@gofastpath.com' -adminFirstName 'Nelson' -adminLastName 'Middendorff'
-.\NewAdmin.ps1 -tenantId $tenantId -adminEmailAddress 'ben.meredith@gofastpath.com' -adminFirstName 'Ben' -adminLastName 'Meredith'
-.\NewAdmin.ps1 -tenantId $tenantId -adminEmailAddress 'zach.messerschmidt@gofastpath.com' -adminFirstName 'Zach' -adminLastName 'Messerschmidt'
-.\NewAdmin.ps1 -tenantId $tenantId -adminEmailAddress 'zach.wear@gofastpath.com' -adminFirstName 'Zach' -adminLastName 'Wear'
-.\NewAdmin.ps1 -tenantId $tenantId -adminEmailAddress 'meyer@gofastpath.com' -adminFirstName 'Alex' -adminLastName 'Meyer'
+.\NewAdmin.ps1 -tenantId $tenantId -adminEmailAddress 'ethan.hartman@gofastpath.com' -adminFirstName 'Ethan' -adminLastName 'Hartman' -idp 'AzureAD' -idpUserId 'ethan.hartman@gofastpath.com'
+.\NewAdmin.ps1 -tenantId $tenantId -adminEmailAddress 'ben.meredith@gofastpath.com' -adminFirstName 'Ben' -adminLastName 'Meredith' -idp 'AzureAD' -idpUserId 'ben.meredith@gofastpath.com'
+.\NewAdmin.ps1 -tenantId $tenantId -adminEmailAddress 'zach.messerschmidt@gofastpath.com' -adminFirstName 'Zach' -adminLastName 'Messerschmidt' -idp 'AzureAD' -idpUserId 'zach.messerschmidt@gofastpath.com'
+.\NewAdmin.ps1 -tenantId $tenantId -adminEmailAddress 'zach.wear@gofastpath.com' -adminFirstName 'Zach' -adminLastName 'Wear' -idp 'AzureAD' -idpUserId 'zach.wear@gofastpath.com'
+.\NewAdmin.ps1 -tenantId $tenantId -adminEmailAddress 'meyer@gofastpath.com' -adminFirstName 'Alex' -adminLastName 'Meyer' -idp 'AzureAD' -idpUserId 'meyer@gofastpath.com'
+.\NewAdmin.ps1 -tenantId $tenantId -adminEmailAddress 'matthew.smith@gofastpath.com' -adminFirstName 'Matt' -adminLastName 'Smith' -idp 'AzureAD' -idpUserId 'matthew.smith@gofastpath.com'
